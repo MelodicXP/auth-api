9d8370feadd84b7acd0b869b475b57cb40c764e7
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-03-07 10:46:26 -0600

    Merge file structure of auth-server and api-server

--- src/models/index.js
+++ src/models/index.js
@@ -16,3 +16,18 @@ module.exports = {
   food: new Collection(food),
   clothes: new Collection(clothes),
 };
+
+
+// 'use strict';
+
+// const userModel = require('./users.js');
+// const { Sequelize, DataTypes } = require('sequelize');
+
+// const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory;';
+
+// const sequelize = new Sequelize(DATABASE_URL);
+
+// module.exports = {
+//   db: sequelize,
+//   users: userModel(sequelize, DataTypes),
+// };
